9fea589401c5e336b280626d2d544dce0ec7923e
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2021-05-31 20:40:01 -0300

    add: bottom navigator

--- App.tsx
+++ App.tsx
@@ -1,30 +1,35 @@
-import React from "react";
+import 'react-native-gesture-handler';
+import React from 'react';
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
-import { ThemeProvider } from "styled-components";
+} from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
 
-import theme from "./src/global/styles/theme";
-import { Register } from "./src/screens/Register";
+import theme from './src/global/styles/theme';
+import { AppRoutes } from './src/routes/app.routes';
 
-export default function App() {
+export default function App(): JSX.Element {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
   });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />
-  // }
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Platform } from 'react-native';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components/native';
+
+import { Dashboard } from '../screens/Dashboard';
+import { Register } from '../screens/Register';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes(): JSX.Element {
+  const theme = useTheme();
+  return (
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 88,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="format-list-bulleted" />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="attach-money" />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="pie-chart" />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
